27c2ade95342d90b05b11a0cd44199d01f845004
Natalia Matioshko <165213062+NatalinaMatioshko@users.noreply.github.com>
2025-01-12 11:43:10 +0200

    add 409 error validation to register user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,6 +1,12 @@
+import createHttpError from 'http-errors';
 import * as services from '../services/users.js';
 
 export const registerUser = async (req, res) => {
+  const findedUser = await services.findUserByEmail(req.body.email);
+  if (findedUser) {
+    throw createHttpError(409, 'User is already exist.');
+  }
+
   const user = await services.registerUser(req.body);
   res.status(201).json({
     status: 201,
--- src/services/users.js
+++ src/services/users.js
@@ -3,3 +3,6 @@ import { UserCollection } from '../db/models/user.js';
 export const registerUser = (userData) => {
   return UserCollection.create(userData);
 };
+export const findUserByEmail = async (email) => {
+  return UserCollection.findOne({ email });
+};
